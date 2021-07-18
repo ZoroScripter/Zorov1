d168b83b464a858e56aeafbf5ca2e920b900d816
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-07-18 11:03:52 -0300

    Zorov1
    git clone https://github.com/ZoroScripter/Zorov1
    cd Zorov1
    python3 Zoro.py

---
+++ Zorov1.py
@@ -0,0 +1,152 @@
+#Codado pir Zoro Novo nick de h4ck3r 777
+#Bibliotecas:
+import os
+import time
+
+
+#\033[1;30m Branco
+#\033[1;31m Vermelho
+#\033[1;32m Verde
+#\033[1;33m Amarelo
+#\033[1;34m Azul
+#\033[1;35m Roxo
+#\033[1;36m Ciano
+#\033[1;37m Cinza Claro
+
+
+os.system("clear")
+
+print("""\033[1;35m
+====================
+l  Zoro Scripter   l
+====================
+""")
+print("""\033[1;34m
+l======================
+l   CTRL + C = EXIT   l
+l=====================l
+l   [1] Ddos-Attack   l
+l   [2] XERXES        l
+l   [3] DDOS          l
+l   [4] HAMMER        l
+l   [5] Trojans       l
+l   [6] Lazy          l
+l   [7] LITEDDOS      l
+l   [8] RED HAWK      l
+l   [8] Snyper        l
+l   [9] Jdash         l
+l   [10] Kali Linux   l
+l   [11] Tbomb        l
+l   [12] Tool-X       l
+l   [13] FSOCIETY     l
+l   [14] Ferramentas  l
+l   [15] HackScript   l
+l   [16] Corlux       l
+l   [17] Git Via Git  l
+l   [18] Apoio        l
+l   [19] Exit         l
+l=====================l
+""")
+op = input(">>> ")
+
+if op == "1":
+    os.system("clear")
+    os.system("git clone https://github.com/Ha3MrX/DDos-Attack")
+    os.system("python3 Zoro.py")
+elif op == "2":
+    os.system("clear")
+    os.system("git clone https://github.com/XCHADXFAQ77X/XERXES")
+    os.system("python3 Zoro.py")
+elif op == "3":
+    os.system("clear")
+    os.system("git clone https://github.com/pembriahmad/DDOS")
+    os.system("python3 Zoro.py")
+elif op == "4":
+    os.system("clear")
+    os.system("git clone https://github.com/cyweb/hammer")
+    os.system("python3 Zoro.py")
+elif op == "5":
+    os.system("clear")
+    os.system("git clone https://github.com/R133F/Trojans")
+    os.system("python3 Zoro.py")
+elif op == "6":
+    os.system("clear")
+    os.system("git clone https://github.com/TechnicalMujeeb/Termux-Lazyscript.git")
+    os.system("python3 Zoro.py")
+elif op == "7":
+    os.system("clear")
+    os.system("git clone https://github.com/4L13199/LITEDDOS")
+    os.system("python3 Zoro.py")
+elif op == "8":
+    os.system("clear")
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+    os.system("python3 Zoro.py")
+elif op == "9":
+    os.systen("clear")
+    os.system("git clone https://github.com/sachin175638/jdash")
+    os.system("python3 Zoro.py")
+elif op == "10":
+    os.system("clear")
+    os.system("git clone https://github.com/noobfoda2/tool")
+    os.system("python3 Zoro.py")
+elif op == "11":
+    os.system("clear")
+    os.system("git clone https://github.com/TheSpeedX/TBomb.git")
+    os.system("python3 Zoro.py")
+elif op == "12":
+    os.system("clear")
+    os.system("git clone https://github.com/rajkumardusad/Tool-X.git")
+    os.system("python3 Zoro.py")
+elif op == "13":
+    os.system("clear")
+    os.system("git clone https://github.com/Manisso/fsociety")
+    os.system("python3 Zoro.py")
+elif op == "14":
+    os.system("clear")
+    os.system("https://github.com/H4ck3r77777/Ferramentas.git")
+    os.system("python3 Zoro.py")
+elif op == "15":
+    os.system("clear")
+    os.system("git clone https://github.com/H4ck3r77777/HackScriptv1.git")
+    os.system("python3 Zoro.py")
+elif op == "16":
+    os.system("clear")
+    os.system("git clone https://github.com/mf256010/Corlux")
+    os.system("python3 Zoro.py")
+elif op == "17":
+    os.system("clear")
+    os.system("git clone https://github.com/git/git")
+    os.system("python3 Zoro.py")
+elif op == "18":
+    print("""\033[1;32m
+    ========
+    = Zoro =
+    ========
+    """)
+    time.sleep(1)
+    print("""\033[1;37m
+    =========
+    =Criador=
+    =========
+    """)
+    time.sleep(1)
+    print("h4ck3r 777")
+    time.sleep(1)
+    print("=============")
+    time.sleep(1)
+    print("Apoiadores:")
+    time.sleep(1)
+    print("==============")
+    time.sleep(1)
+    print("Columbia")
+    time.sleep(1)
+    print("w4li")
+    time.sleep(4)
+    os.system("python3 Zoro.py")
+elif op == "19":
+    os.system("clear")
+    print("Finalizando...")
+    time.sleep(3)
+    exit()
+else:
+    os.system("python3 Zoro.py")
